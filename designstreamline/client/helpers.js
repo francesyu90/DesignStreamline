df70dcf9656dadfe8272cadcafcfefae9281b64b
francesyu90 <francesyu90@yahoo.com>
2017-11-25 17:12:05 -0800

    Implemented feature: getting products

--- designstreamline/client/helpers.js
+++ designstreamline/client/helpers.js
@@ -3,6 +3,7 @@ Template.navbar.helpers({
     'projectName': function() {
     	return "Design Streamline";
     }
+
 });
 
 
@@ -11,4 +12,27 @@ Template.footer.helpers({
     'year': function() {
     	return new Date().getFullYear();
     }
+
+});
+
+Template.catalog.helpers({
+
+	'productList': function() {
+		var productList = Products.find().fetch();
+		if(productList.length == 0)
+			return;
+		var products = [];
+		for(var i = 0; i < productList.length; i++) {
+
+			var product = {
+				"productName": productList[i].productName,
+				"_id": productList[i]._id,
+				"componentName": Components.findOne({"_id": productList[i].componentID}).componentName,
+				"featureName": Features.findOne({"_id": productList[i].featureID}).featureName
+			}
+			products.push(product);
+		}
+		return products;
+	}
+
 });
