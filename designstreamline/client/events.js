b4e1279143143c766a3788c10cf3d9cec6601f66
francesyu90 <francesyu90@yahoo.com>
2017-11-25 23:37:43 -0800

    Fixed bugs

--- designstreamline/client/events.js
+++ designstreamline/client/events.js
@@ -1,77 +1,3 @@
-Template.create.events({
-
-    'submit .createProject'(event, instance) {
-	   	var seasonName = event.target.seasonName.value;
-	   	var lifeCycle = event.target.lifeCycle.value;
-	   	var re = /\s*;\s*/;
-	   	var productList = event.target.products.value.split(re);
-	   	var featureList = event.target.features.value.split(re);
-	   	var componentList = event.target.components.value.split(re);
-
-	   	var featureKeyList = [];
-
-	   	for(var i = 0; i < featureList.length; i++) {
-
-	   		var feature = {
-	   			"featureName": featureList[i]
-	   		};
-
-	   		var featureKey = Features.insert(feature);
-	   		featureKeyList.push(featureKey);
-	   	}
-
-	   	var componentKeyList = [];
-
-	   	for(var i = 0; i < componentList.length; i++) {
-
-	   		var component = {
-	   			"componentName": componentList[i]
-	   		}
-	   		var componentKey = Components.insert(component);
-	   		componentKeyList.push(componentKey);
-
-	   	}
-
-	   	var productKeyList = [];
-
-	   	for(var i = 0; i < productList.length; i++) {
-
-
-	   		for(var x = 0; x < featureKeyList.length; x++) {
-
-
-	   			for(var y = 0; y < componentKeyList.length; y++) {
-
-	   				var product = {
-	   					"productName": productList[i],
-	   					"featureID": featureKeyList[x],
-	   					"componentID": componentKeyList[y]
-	   				}
-
-	   				var productKey = Products.insert(product);
-	   				productKeyList.push(productKey);
-
-	   			}
-
-	   		}
-
-	   	}
-
-	   	for(var i = 0; i < productKeyList.length; i++) {
-
-	   		var project = {
-	   			"seasonName": seasonName,
-	   			"lifeCycle": lifeCycle,
-	   			"productID": productKeyList[i]
-	   		};
-
-	   		Projects.insert(project);
-	   	}
-
-	}
-});
-
-
 Template.form.events({
 
 	'change #projectPicker'(event, instance) {
@@ -108,10 +34,14 @@ Template.form.events({
 
 Template.pill.events({
 
-	'click .nav-item'(event, instance) {
-		console.log(event.target);
-		$(this).addClass('active')
-		$(this).siblings.removeClass('active');
+	'click .nav-link'(event, instance) {
+		$(".nav-link").removeClass('active');
+		$(".tab-pane").removeClass('active');
+		$(".tab-pane").removeClass('show');
+		$(this).addClass('active');
+		$(event.target).addClass('active');
+		$(event.target).addClass('show');
+
 	}
 
 });
--- designstreamline/client/main.js
+++ designstreamline/client/main.js
@@ -24,3 +24,76 @@ import '../lib/collections.js';
 //     Projects.insert({ name: "Project1", desc: "Description1" });
 //   },
 // });
+
+// Template.create.events({
+
+//     'submit .createProject'(event, instance) {
+// 	   	var seasonName = event.target.seasonName.value;
+// 	   	var lifeCycle = event.target.lifeCycle.value;
+// 	   	var re = /\s*;\s*/;
+// 	   	var productList = event.target.products.value.split(re);
+// 	   	var featureList = event.target.features.value.split(re);
+// 	   	var componentList = event.target.components.value.split(re);
+
+// 	   	var featureKeyList = [];
+
+// 	   	for(var i = 0; i < featureList.length; i++) {
+
+// 	   		var feature = {
+// 	   			"featureName": featureList[i]
+// 	   		};
+
+// 	   		var featureKey = Features.insert(feature);
+// 	   		featureKeyList.push(featureKey);
+// 	   	}
+
+// 	   	var componentKeyList = [];
+
+// 	   	for(var i = 0; i < componentList.length; i++) {
+
+// 	   		var component = {
+// 	   			"componentName": componentList[i]
+// 	   		}
+// 	   		var componentKey = Components.insert(component);
+// 	   		componentKeyList.push(componentKey);
+
+// 	   	}
+
+// 	   	var productKeyList = [];
+
+// 	   	for(var i = 0; i < productList.length; i++) {
+
+
+// 	   		for(var x = 0; x < featureKeyList.length; x++) {
+
+
+// 	   			for(var y = 0; y < componentKeyList.length; y++) {
+
+// 	   				var product = {
+// 	   					"productName": productList[i],
+// 	   					"featureID": featureKeyList[x],
+// 	   					"componentID": componentKeyList[y]
+// 	   				}
+
+// 	   				var productKey = Products.insert(product);
+// 	   				productKeyList.push(productKey);
+
+// 	   			}
+
+// 	   		}
+
+// 	   	}
+
+// 	   	for(var i = 0; i < productKeyList.length; i++) {
+
+// 	   		var project = {
+// 	   			"seasonName": seasonName,
+// 	   			"lifeCycle": lifeCycle,
+// 	   			"productID": productKeyList[i]
+// 	   		};
+
+// 	   		Projects.insert(project);
+// 	   	}
+
+// 	}
+// });
